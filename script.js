e9539dd0cc4fe8ae327a3281f444b5715267512f
Aman Mittal <mittalaman797@gmail.com>
2025-11-13 15:00:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-//your JS code here. If required.
+
 const inputs = document.querySelectorAll(".code");
 
 inputs.forEach((input, index) => {
   input.addEventListener("input", (e) => {
     const value = e.target.value;
 
+    // allow only digits
     if (value.match(/[0-9]/)) {
       e.target.value = value.slice(-1);
       if (index < inputs.length - 1) {
         inputs[index + 1].focus();
       }
     } else {
-      e.target.value = ""; // allow only digits
+      e.target.value = "";
     }
   });
 
@@ -29,6 +30,6 @@ inputs.forEach((input, index) => {
   });
 });
 
-// Auto-focus first input on page load
+// focus on first input on page load
 window.addEventListener("load", () => inputs[0].focus());
 
